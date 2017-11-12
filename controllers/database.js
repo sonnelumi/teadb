76f0056c1aa7330ac09c6fa4c89a1e83b9a3238c
Sharon Wong <sharonw@wc-3.com>
2017-11-12 13:05:40 -0800

    mongodb

---
+++ controllers/database.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+// body parser
+var bodyParser = require('body-parser');
+var path = require ('path');
+var querystring = require('querystring'); // get query string
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://teadb:dbteapass@ds149905.mlab.com:49905/heroku_bl98vszh';
+
+module.exports.getAllOrders = function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
+
+        //get collection of routes
+        var Routes = db.collection('Orders');
+
+
+        //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
+        //   this request and loop  is to display content in the  console log
+        var c = Routes.find({});
+
+        c.forEach(
+            function(myDoc) {
+                console.log( "name: " + myDoc.name );  //just  loging the output to the console
+            }
+        );
+
+
+        // SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
+        //   array called docs that you  forward to the  getAllRoutes.ejs view for use there
+        Routes.find().toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('getAllRoutes', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+};//end function
